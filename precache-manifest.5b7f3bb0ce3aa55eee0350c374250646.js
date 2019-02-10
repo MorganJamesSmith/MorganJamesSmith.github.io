6e7f4b310f53002e785f4fae3bf266b127cda94e
MorganJamesSmith <Morgan.J.Smith@outlook.com>
2019-02-10 11:16:19 -0500

    init commit

---
+++ precache-manifest.5b7f3bb0ce3aa55eee0350c374250646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a4d3aaf51da84d1f718",
+    "url": "/static/js/main.0a4d3aaf.chunk.js"
+  },
+  {
+    "revision": "5fc8a94cf5e465ec76ab",
+    "url": "/static/js/1.5fc8a94c.chunk.js"
+  },
+  {
+    "revision": "0a4d3aaf51da84d1f718",
+    "url": "/static/css/main.541b8790.chunk.css"
+  },
+  {
+    "revision": "e295a00300d116f464a010ebe44e305c",
+    "url": "/index.html"
+  }
+];
